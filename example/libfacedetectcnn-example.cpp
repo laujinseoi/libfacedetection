dbedf45598a406f04dde14e1c51b7ccc687802e5
laujinseoi <laujinseoi@yeah.net>
2019-03-14 15:35:31 +0800

    Input image from camera

--- example/libfacedetectcnn-example.cpp
+++ example/libfacedetectcnn-example.cpp
@@ -33,58 +33,64 @@ using namespace cv;
 
 int main(int argc, char* argv[])
 {
-    if(argc != 2)
-    {
-        printf("Usage: %s <image_file_name>\n", argv[0]);
-        return -1;
-    }
+    // if(argc != 2)
+    // {
+    //     printf("Usage: %s <image_file_name>\n", argv[0]);
+    //     return -1;
+    // }
 
-	//load an image and convert it to gray (single-channel)
-	Mat image = imread(argv[1]); 
-	if(image.empty())
-	{
-		fprintf(stderr, "Can not load the image file %s.\n", argv[1]);
-		return -1;
-	}
+	// //load an image and convert it to gray (single-channel)
+	// Mat image = imread(argv[1]); 
+	// if(image.empty())
+	// {
+	// 	fprintf(stderr, "Can not load the image file %s.\n", argv[1]);
+	// 	return -1;
+	// }
 
 	int * pResults = NULL; 
-    //pBuffer is used in the detection functions.
-    //If you call functions in multiple threads, please create one buffer for each thread!
+    // //pBuffer is used in the detection functions.
+    // //If you call functions in multiple threads, please create one buffer for each thread!
     unsigned char * pBuffer = (unsigned char *)malloc(DETECT_BUFFER_SIZE);
     if(!pBuffer)
     {
         fprintf(stderr, "Can not alloc buffer.\n");
         return -1;
     }
+	VideoCapture cap(0);
+	while(1)
+	{
+				///////////////////////////////////////////
+		// CNN face detection 
+		// Best detection rate
+		//////////////////////////////////////////
+		//!!! The input image must be a RGB one (three-channel)
+		//!!! DO NOT RELEASE pResults !!!
+		Mat image;
+		cap >> image;
+		pResults = facedetect_cnn(pBuffer, (unsigned char*)(image.ptr(0)), image.cols, image.rows, (int)image.step);
+
+		printf("%d faces detected.\n", (pResults ? *pResults : 0));
+		Mat result_cnn = image.clone();;
+		//print the detection results
+		for(int i = 0; i < (pResults ? *pResults : 0); i++)
+		{
+			short * p = ((short*)(pResults+1))+142*i;
+			int x = p[0];
+			int y = p[1];
+			int w = p[2];
+			int h = p[3];
+			int neighbors = p[4];
+			int angle = p[5];
+
+			printf("face_rect=[%d, %d, %d, %d], neighbors=%d, angle=%d\n", x,y,w,h,neighbors, angle);
+			rectangle(result_cnn, Rect(x, y, w, h), Scalar(0, 255, 0), 2);
+		}
+		imshow("result_cnn", result_cnn);
+		waitKey(1);
+	}
 	
 
-	///////////////////////////////////////////
-	// CNN face detection 
-	// Best detection rate
-	//////////////////////////////////////////
-	//!!! The input image must be a RGB one (three-channel)
-	//!!! DO NOT RELEASE pResults !!!
-	pResults = facedetect_cnn(pBuffer, (unsigned char*)(image.ptr(0)), image.cols, image.rows, (int)image.step);
 
-    printf("%d faces detected.\n", (pResults ? *pResults : 0));
-	Mat result_cnn = image.clone();;
-	//print the detection results
-	for(int i = 0; i < (pResults ? *pResults : 0); i++)
-	{
-        short * p = ((short*)(pResults+1))+142*i;
-		int x = p[0];
-		int y = p[1];
-		int w = p[2];
-		int h = p[3];
-		int neighbors = p[4];
-		int angle = p[5];
-
-		printf("face_rect=[%d, %d, %d, %d], neighbors=%d, angle=%d\n", x,y,w,h,neighbors, angle);
-		rectangle(result_cnn, Rect(x, y, w, h), Scalar(0, 255, 0), 2);
-	}
-	imshow("result_cnn", result_cnn);
-
-	waitKey();
 
     //release the buffer
     free(pBuffer);
